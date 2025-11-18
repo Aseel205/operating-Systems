c9a2ee9b80de15715f5c90541cd44b0d55dca6c2
Aseel Biadsy <aseelbiadse1@gmail.com>
2025-11-18 13:16:34 +0200

    Delete Assigment2-lokcsImplementaion/xv6-riscv-riscv directory

--- Assigment2-lokcsImplementaion/xv6-riscv-riscv/kernel/petersonlock.h
+++
@@ -1,7 +0,0 @@
-// kernel/petersonlock.h
-
-struct petersonlock {
-  int flag[2]; // Flags for each role (0 or 1)
-  int turn;    // Whose turn it is
-  int active;  // 1 = active, 0 = destroyed
-};
